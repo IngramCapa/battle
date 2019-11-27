cb5be03097319c3fce2574a39b05606cafb9c4d5
Raluca Ciucu <raluca.ciucu@gmail.com>
2019-11-27 16:09:51 +0000

    Feature test getting player names

--- app.rb
+++ app.rb
@@ -3,9 +3,16 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
     
     get '/' do
-        "hello battle"
-        "Testing infrastructure working!"
+        erb(:index)
     end
+
+    post '/names' do
+        @player_1 = params[:Player_1]
+        @player_2 = params[:Player_2]
+        erb(:play)
+
+    end
+
 
     # start server if ruby file executed directly
     run! if app_file == $0
